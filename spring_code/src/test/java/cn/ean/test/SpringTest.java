70e2da2d0e95c7236e944bff26a80a1bbbd842d8
BlackSuit <ean969648839@163.com>
2021-09-20 23:34:10 +0800

    Initial commit

---
+++ spring_code/src/test/java/cn/ean/test/SpringTest.java
@@ -0,0 +1,12 @@
+package cn.ean.test;
+
+import org.graalvm.compiler.serviceprovider.ServiceProvider;
+import org.junit.Test;
+
+
+public class SpringTest {
+    @Test
+    public void testSpring(){
+
+    }
+}
